ccf0fb3fa35731f9cb25f0f33c21fdb8440d4178
Tatiana Hodic <tatiana.hodic.miklos@gmail.com>
2022-09-29 20:49:06 +0200

    done

--- src/components/Offline/index.jsx
+++ src/components/Offline/index.jsx
@@ -3,6 +3,28 @@ import React, { useEffect, useState } from 'react';
 const Offline = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
 
+  const handleOnline = () => {
+    setIsOnline(true);
+  }
+
+  const handleOffline = () => {
+    setIsOnline(false);
+  }
+
+  useEffect(() => {
+    console.log('mounted');
+    window.addEventListener('online', handleOnline)
+    window.addEventListener('offline', handleOffline)
+
+    return () => {
+      console.log('unmounted');
+      window.addEventListener('online', handleOnline)
+      window.addEventListener('offline', handleOffline)
+    }
+  },
+  []
+  );
+
   if (isOnline) {
     return (
       <p>Všechno v pořádku</p>
